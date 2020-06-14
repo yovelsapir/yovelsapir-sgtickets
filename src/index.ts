565f4e9a86fd2447da1607a063ba863e15fa3fa9
yovelsapir <yovelsapir@gmail.com>
2020-06-14 13:11:05 +0300

    additional config

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+   red: number;
+   blue: number;
+   green: number;
+}
+
+const color: Color = {
+   red: 10,
+   blue: 20,
+   green: 50,
+};
+
+console.log(color);
+
+export default color;
