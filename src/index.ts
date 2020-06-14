e090c4c553a49f163199a9b196560a286617542e
yovelsapir <yovelsapir@gmail.com>
2020-06-14 13:28:09 +0300

    Updates

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ export * from './errors/database-connection-error';
 export * from './errors/not-authorized-error';
 export * from './errors/not-found-error';
 export * from './errors/request-validation-error';
-
 export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
